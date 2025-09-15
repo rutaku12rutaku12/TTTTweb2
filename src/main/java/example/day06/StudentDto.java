23629404d114d6bd7e80ef04cd297b6f848168ee
rutaku12rutaku12 <chookyuho@gmail.com>
2025-09-15 17:32:44 +0900

    리액트, 마이바티스 커밋

---
+++ src/main/java/example/day06/StudentDto.java
@@ -0,0 +1,12 @@
+package example.day06;
+
+import lombok.*;
+
+@Setter @Getter @ToString @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class StudentDto {
+    private int sno;
+    private String name;
+    private int kor;
+    private int math;
+}
---
+++ src/main/resources/static/day06/math.js
@@ -0,0 +1,6 @@
+// math.js
+// 1. 함수 선언
+const add = ( a , b ) => { return a+b }
+// 2. 함수 (기본) 내보내기 , export default 자료
+// 해당 파일을 import 했을때 기본적으로 반환되는 자료   
+export default add;
